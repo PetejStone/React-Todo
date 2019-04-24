4586e908175ace0cfc01d0c13049ce47cc8f683f
PetejStone <petejstoney@gmail.com>
2019-04-24 12:35:45 -0700

    created todo array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+const todoData =  [
+  {
+    task: 'Clean Room',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Get Oil Change',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Get a Job',
+    id: Date.now(),
+    completed: false
+  }
+]
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // this component is going to take care of state, and any chang e handlers you need to work with your state
   render() {
     return (
       <div>
